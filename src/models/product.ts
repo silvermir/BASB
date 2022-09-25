c653c6fbb87b7f8d5c19bfc774e37d5e7e382143
Ugur Erdemir <ugur.erdemir@mercedes-benz.com>
2022-09-25 12:26:11 +0200

    added products model

---
+++ src/models/product.ts
@@ -0,0 +1,60 @@
+import Client from '../database'
+
+export type Product = {
+    id: number;
+    product_name: string;
+    price: number;
+    category: string;
+}
+
+export class ProductStore {
+    async index(): Promise<Product[]>{
+        try{
+            const conn = await Client.connect();
+            const sql = 'SELECT * FROM products';
+            const result = await conn.query(sql);
+            conn.release();
+            return result.rows
+        }catch (e) {
+            throw new Error (`cant get product ${e}`)
+        }
+    }
+
+
+    async show(id: string): Promise<Product>{
+        try{
+            const conn = await Client.connect();
+            const sql = 'SELECT * FROM products WHERE id=($1)';
+            const result = await conn.query(sql, [id]);
+            conn.release();
+            return result.rows[0]
+        }catch (e) {
+            throw new Error (`cant get product ${id}. Error: ${e}`)
+        }
+    }
+
+    async create(p: Product): Promise<Product>{
+        try{
+            const conn = await Client.connect();
+            const sql = 'INSERT INTO products(id, name, price, category) VALUES($1, $2, $3, $4) RETURNING *';
+            const result = await conn.query(sql, [p.id, p.product_name, p.price, p.category]);
+            conn.release();
+            return result.rows[0]
+        }catch (e) {
+            throw new Error (`cant get product ${p.product_name}. Error: ${e}`)
+        }
+    }
+
+    async delete(id: string): Promise<Product>{
+        try{
+            const conn = await Client.connect();
+            const sql = 'DELETE FROM products WHERE id=($1)';
+            const result = await conn.query(sql, [id]);
+            conn.release();
+            return result.rows[0]
+        }catch (e) {
+            throw new Error (`cant delete product ${id}. Error: ${e}`)
+        }
+    }
+
+}
