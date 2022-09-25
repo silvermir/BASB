939458dffbbf32392d4845015afd0f26958560eb
Ugur Erdemir <ugur.erdemir@mercedes-benz.com>
2022-09-25 22:18:33 +0200

    added jwt

--- src/models/user.ts
+++ src/models/user.ts
@@ -1,6 +1,6 @@
 import Client from "../database";
-import bcrypt from 'bcrypt'
-import dotenv from 'dotenv'
+import bcrypt from "bcrypt";
+import dotenv from "dotenv";
 
 dotenv.config();
 const { pepper, saltRounds } = process.env;
@@ -15,68 +15,69 @@ export type User = {
 };
 
 export class UserStore {
-    async index(): Promise<User[]> {
-        try {
-          const conn = await Client.connect();
-          const sql = "SELECT * FROM users";
-          const result = await conn.query(sql);
-          conn.release();
-          return result.rows;
-        } catch (e) {
-          throw new Error(`cant get user ${e}`);
-        }
-      }
-      async show(id: string): Promise<User> {
-        try {
-          const conn = await Client.connect();
-          const sql = "SELECT * FROM products WHERE id=($1)";
-          const result = await conn.query(sql, [id]);
-          conn.release();
-          return result.rows[0];
-        } catch (e) {
-          throw new Error(`cant get user ${id}. Error: ${e}`);
-        }
-      }
-
-async create(u:User): Promise<User> {
+  async index(): Promise<User[]> {
     try {
-      const conn = await Client.connect()
-      const sql = 'INSERT INTO users (username, password_digest) VALUES($1, $2) RETURNING *'
+      const conn = await Client.connect();
+      const sql = "SELECT * FROM users";
+      const result = await conn.query(sql);
+      conn.release();
+      return result.rows;
+    } catch (e) {
+      throw new Error(`cant get user ${e}`);
+    }
+  }
+  async show(id: string): Promise<User> {
+    try {
+      const conn = await Client.connect();
+      const sql = "SELECT * FROM users WHERE id=($1)";
+      const result = await conn.query(sql, [id]);
+      conn.release();
+      return result.rows[0];
+    } catch (e) {
+      throw new Error(`cant get user ${id}. Error: ${e}`);
+    }
+  }
+
+  async create(u: User): Promise<User> {
+    try {
+      const conn = await Client.connect();
+      const sql =
+        "INSERT INTO users (username, password_digest) VALUES($1, $2) RETURNING *";
 
       const hash = bcrypt.hashSync(
-        u.password_digest! + pepper, 
+        u.password_digest! + pepper,
         parseInt(saltRounds!)
       );
 
-      const result = await conn.query(sql, [u.username, hash])
-      const user = result.rows[0]
+      const result = await conn.query(sql, [u.username, hash]);
+      const user = result.rows[0];
 
-      conn.release()
+      conn.release();
 
-      return user
-    } catch(err) {
-      throw new Error(`error creating user (${u.username}): ${err}`)
-    } 
-}
+      return user;
+    } catch (err) {
+      throw new Error(`error creating user (${u.username}): ${err}`);
+    }
+  }
 
-async authenticate(username: string, password: string): Promise<User | null> {
-    const conn = await Client.connect()
-    const sql = 'SELECT password_digest FROM users WHERE username=($1)'
+  async authenticate(username: string, password: string): Promise<User | null> {
+    const conn = await Client.connect();
+    const sql = "SELECT password_digest FROM users WHERE username=($1)";
 
-    const result = await conn.query(sql, [username])
+    const result = await conn.query(sql, [username]);
 
-    console.log(password+pepper)
+    console.log(password + pepper);
 
-    if(result.rows.length) {
+    if (result.rows.length) {
+      const user = result.rows[0];
 
-      const user = result.rows[0]
+      console.log(user);
 
-      console.log(user)
-
-      if (bcrypt.compareSync(password+pepper, user.password_digest)) {
-        return user
+      if (bcrypt.compareSync(password + pepper, user.password_digest)) {
+        return user;
       }
     }
 
-    return null
+    return null;
   }
+}
